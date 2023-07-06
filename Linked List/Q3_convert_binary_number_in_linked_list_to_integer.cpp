9bdb0735c07b9de88a94e3ce439f14c3c82a3684
Debanjan Poddar <debanjan2602@gmail.com>
2023-07-06 22:29:53 +0530

    Added Linked List Q3

---
+++ Linked List/Q3_convert_binary_number_in_linked_list_to_integer.cpp
@@ -0,0 +1,44 @@
+// Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+// Return the decimal value of the number in the linked list.
+
+// The most significant bit is at the head of the linked list.
+
+// Example 1:
+// Input: head = [1,0,1]
+// Output: 5
+// Explanation: (101) in base 2 = (5) in base 10
+
+// Example 2:
+// Input: head = [0]
+// Output: 0
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        ListNode* runner = head;
+        int ans = 0;
+        while(runner){
+            ans = ans * 2;
+            if(runner->val == 1){
+                ans += 1;
+            }
+            runner = runner->next;
+        }
+        return ans;
+    }
+};
+
+// Time Complexity  : O(n)
+// Space Complexity : O(1)
