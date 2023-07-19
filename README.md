9e1936e0fb654010a4b7a8fed8a4936c9eab7085
Debanjan Poddar <debanjan2602@gmail.com>
2023-07-19 23:59:49 +0530

    Added Trees Q28

--- README.md
+++ README.md
@@ -104,4 +104,5 @@
 **Q24.** [Binary Tree Right Side View](https://leetcode.com/problems/binary-tree-right-side-view/) [(Solution - BFS - V.IMP)](/Trees/Q24_binary_tree_right_side_view.cpp)\
 **Q25.** [Redundant Connection](https://leetcode.com/problems/redundant-connection/description/) [(Solution - DSU - IMP)](/Trees/Q25_redundant_connection.cpp)\
 **Q26.** [Binary Tree Level Order Traversal](https://leetcode.com/problems/binary-tree-level-order-traversal/description/) [(Solution)](/Trees/Q26_binary_tree_level_order_traversal.cpp)\
-**Q27.** [Path Sum III](https://leetcode.com/problems/path-sum-iii/description/) [(Solution - DFS + Hashing - V.V.IMP)](/Trees/Q27_path_sum_III.cpp)
+**Q27.** [Path Sum III](https://leetcode.com/problems/path-sum-iii/description/) [(Solution - DFS + Hashing - V.V.IMP)](/Trees/Q27_path_sum_III.cpp)\
+**Q28.** [Construct Binary Tree from Preorder and Postorder Traversal](https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/description/) [(Solution - V.IMP)](/Trees/Q28_construct_binary_tree_from_preorder_and_postorder_traversal.cpp)
---
+++ Trees/Q28_construct_binary_tree_from_preorder_and_postorder_traversal.cpp
@@ -0,0 +1,49 @@
+// Given two integer arrays, preorder and postorder where preorder is the preorder traversal of a binary tree of distinct values and postorder is the postorder traversal of the same tree, reconstruct and return the binary tree.
+
+// If there exist multiple answers, you can return any of them.
+
+// Example 1:
+// Input: preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1]
+// Output: [1,2,3,4,5,6,7]
+
+// Example 2:
+// Input: preorder = [1], postorder = [1]
+// Output: [1]
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+    unordered_map<int,int> m;
+public:
+    TreeNode* dfs(vector<int>& pre, vector<int>& post, int preStart, int preEnd, int postStart, int postEnd){
+        if(preStart > preEnd)
+            return NULL;
+        TreeNode* root = new TreeNode(pre[preStart]);
+        if(preStart == preEnd)
+            return root;
+        
+        int idx = m[pre[preStart + 1]];
+        int offset = idx - postStart;
+        root->left = dfs(pre, post, preStart + 1, preStart + 1 + offset, postStart, idx);
+        root->right = dfs(pre, post, preStart + 1 + offset + 1, preEnd, idx+1, postEnd);
+        return root;
+    }
+    
+    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
+        int n = pre.size();
+        for(int i = 0;i < n;i++)
+            m[post[i]] = i;
+        return dfs(pre, post, 0, n - 1, 0, n - 1);
+    }
+};
